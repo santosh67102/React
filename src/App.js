9dfed8f1276bc4242b7b8ee773970c4e310cca66
Santosh Gautam <santoshgautam@Santoshs-MBP.home>
2019-08-06 00:30:05 -0400

    operate npm run eject

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.css";
 import Person from "./Components/Person";
 
 class App extends Component {
@@ -81,7 +81,7 @@ class App extends Component {
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(" ")}>This is my fisrt react project</p>
           <hr />
